baa439c04ffae4a1c8e77ab832654c47228d9be6
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2010-11-21 11:51:15 +0000

    Added some acceptance test and added them to the suite

--- src/org/baltoaca/conv_valut/test/suites/TestSuiteAcceptanceTests.java
+++ src/org/baltoaca/conv_valut/test/suites/TestSuiteAcceptanceTests.java
@@ -3,9 +3,8 @@ package org.baltoaca.conv_valut.test.suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { TestSuiteLearningTests.class,TestSuiteUnitaryTests.class })
-public class TestSuiteAllTest {
+@Suite.SuiteClasses({TestInputOutput.class})
+public class TestSuiteAcceptanceTests {
 
 }
