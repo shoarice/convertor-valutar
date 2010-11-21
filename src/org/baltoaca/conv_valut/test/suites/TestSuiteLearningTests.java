0404e10a4487a1314c01552e85ad05c23e9ec27b
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2010-11-21 11:03:19 +0000

    Added the test to suites

---
+++ src/org/baltoaca/conv_valut/test/suites/TestSuiteLearningTests.java
@@ -0,0 +1,12 @@
+package org.baltoaca.conv_valut.test.suites;
+
+import org.baltoaca.conv_valut.test.TestFormatterLearning;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {TestFormatterLearning.class})
+public class TestSuiteLearningTests {
+
+}
