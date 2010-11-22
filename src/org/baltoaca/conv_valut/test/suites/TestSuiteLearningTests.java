c42e1a338ce1b7705d7733af93630681188a27c3
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2010-11-22 13:58:26 +0000

    Modified the way the date in XmlInformation is parsed as a String

--- src/org/baltoaca/conv_valut/test/suites/TestSuiteLearningTests.java
+++ src/org/baltoaca/conv_valut/test/suites/TestSuiteLearningTests.java
@@ -1,13 +1,14 @@
 package org.baltoaca.conv_valut.test.suites;
 
+import org.baltoaca.conv_valut.test.TestDateFormatLearning;
 import org.baltoaca.conv_valut.test.TestFormatterLearning;
 import org.baltoaca.conv_valut.test.TestStringLearning;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
-@Suite.SuiteClasses( {TestFormatterLearning.class, TestStringLearning.class})
+@Suite.SuiteClasses({ TestFormatterLearning.class, TestStringLearning.class,
+		TestDateFormatLearning.class })
 public class TestSuiteLearningTests {
 
 }
