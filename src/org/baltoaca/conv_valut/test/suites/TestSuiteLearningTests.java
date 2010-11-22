dd253fa7ce716c3aea2c33735feb98bffb90a5f2
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2010-11-22 13:30:50 +0000

    Added a learning test about String

--- src/org/baltoaca/conv_valut/test/suites/TestSuiteLearningTests.java
+++ src/org/baltoaca/conv_valut/test/suites/TestSuiteLearningTests.java
@@ -1,12 +1,13 @@
 package org.baltoaca.conv_valut.test.suites;
 
 import org.baltoaca.conv_valut.test.TestFormatterLearning;
+import org.baltoaca.conv_valut.test.TestStringLearning;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( {TestFormatterLearning.class})
+@Suite.SuiteClasses( {TestFormatterLearning.class, TestStringLearning.class})
 public class TestSuiteLearningTests {
 
 }
