2b91a0b92bd095dd67835716bc9d4a552129bf73
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-09 17:02:58 +0000

    Modified the test class Main

--- branches/CommandLineParser/src/Main.java
+++ branches/CommandLineParser/src/Main.java
@@ -1,4 +1,5 @@
 import org.baltoaca.CommandLineParser.CommandLineParser;
+import org.baltoaca.CommandLineParser.Executor;
 
 import exceptions.ConsoleNotFoundException;
 import exceptions.ExecutorNotFoundException;
@@ -7,6 +8,13 @@ import exceptions.ExecutorNotFoundException;
 public class Main {
 
 	public static void main(String[] args) throws ConsoleNotFoundException, ExecutorNotFoundException {
+		CommandLineParser.instance().setExecutor(new Executor() {
+			
+			@Override
+			public String executeAndGenerateResponse(String[] commandElements) {
+				return null;
+			}
+		});
 		CommandLineParser.instance().run();
 	}
 
