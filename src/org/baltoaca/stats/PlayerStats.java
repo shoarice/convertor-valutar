51ea17765cff6c1a3e6c6cfc518d7d5cc8166f0e
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-12 08:06:55 +0000

    Added health, mana and level to the PlayerStats class

--- src/org/baltoaca/stats/PlayerStats.java
+++ src/org/baltoaca/stats/PlayerStats.java
@@ -1,6 +1,52 @@
 package org.baltoaca.stats;
 
-public class PlayerStats {
 
+public class PlayerStats {
+	IntervalIntegerValue health;
+	IntervalIntegerValue mana;
+	Level level;
+	
+	public int getMaxHealth(){
+		return health.getMaxValue();
+	}
+	
+	public int getCurrentHealth(){
+		return health.getCurrentValue();
+	}
+	
+	public boolean isHealthZero(){
+		return health.isZero();
+	}
+	
+	public int getMaxMana(){
+		return health.getMaxValue();
+	}
+	
+	public int getCurrentMana(){
+		return health.getCurrentValue();
+	}
+	
+	public boolean isManaZero(){
+		return health.isZero();
+	}
+	
+	/**
+	 * Resets the level up flag when called
+	 */
+	public boolean levelUp(){
+		return level.isLeveledUp();
+	}
+	
+	public int getExperience(){
+		return level.getExperience();
+	}
+	
+	public void addExperience(int amount){
+		level.addExperience(amount);
+	}
+	
+	public int getLevel(){
+		return level.getLevel();
+	}
 
 }
