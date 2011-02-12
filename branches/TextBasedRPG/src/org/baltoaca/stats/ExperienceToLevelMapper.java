3d2289256edd1939f411076e39ce334dc7fcc04a
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-12 08:06:55 +0000

    Added health, mana and level to the PlayerStats class

---
+++ branches/TextBasedRPG/src/org/baltoaca/stats/ExperienceToLevelMapper.java
@@ -0,0 +1,10 @@
+package org.baltoaca.stats;
+
+public class ExperienceToLevelMapper {
+
+	public int getLevelFromExperience(int experience) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ branches/TextBasedRPG/src/org/baltoaca/stats/Level.java
@@ -0,0 +1,54 @@
+package org.baltoaca.stats;
+
+/**
+ * This class represents the leveling system in the game.
+ * 
+ * When the client checks for levelUp() the level up flag will be
+ * reset to false
+ * 
+ * @author VlaD
+ *
+ */
+public class Level {
+	private int experience;
+	private int level;
+	private ExperienceToLevelMapper mapper;
+	private boolean levelUp;
+	
+	public Level(){
+		//TODO init mapper
+	}
+	public void addExperience(int amount){
+		experience+=amount;
+		updateLevelIfNeededAndSetLevelUpFlag();
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+	private void updateLevelIfNeededAndSetLevelUpFlag() {
+		int newLevel = mapper.getLevelFromExperience(experience);
+		if(newLevel > level)
+			this.level = newLevel;
+		
+		levelUp = true;
+	}
+	
+	public int getLevel(){
+		return level;
+		
+	}
+	
+	public boolean isLeveledUp(){
+		return getLevelUpAndSetLevelUpFlag();
+	}
+	
+	private boolean getLevelUpAndSetLevelUpFlag(){
+		try{
+			return levelUp;
+		}finally{
+			levelUp = false;
+		}
+	}
+	
+}
