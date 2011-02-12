b2a11831e8b42982feec028a520afc676a268a5d
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-12 08:54:50 +0000

    Added test suites

---
+++ branches/TextBasedRPG/src/org/baltoaca/tests/UnitTests.java
@@ -0,0 +1,12 @@
+package org.baltoaca.tests;
+
+import org.baltoaca.tests.unit.TestIntervalntegerValue;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestIntervalntegerValue.class})
+public class UnitTests {
+
+}
