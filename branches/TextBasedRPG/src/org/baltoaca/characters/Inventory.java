a2d816269a69d9fd5b608742c9fcc57fed0039b1
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-13 12:36:24 +0000

    Partial implementation of the Inventory class

--- branches/TextBasedRPG/src/org/baltoaca/characters/Inventory.java
+++ branches/TextBasedRPG/src/org/baltoaca/characters/Inventory.java
@@ -1,5 +1,31 @@
 package org.baltoaca.characters;
 
-public class Inventory {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.baltoaca.objects.InvetoryObject;
+
+public class Inventory {
+	Map<InvetoryObject, Integer> objects;
+	int maxSlots;
+	
+	public Inventory(){
+		maxSlots = 10;
+		objects = getMap();
+	}
+	
+	public Inventory(int maxSlots){
+		this();
+		this.maxSlots = maxSlots;
+	}
+	
+	//TODO create the methods that add and remove objects
+	
+	protected Map<InvetoryObject, Integer> getMap(){
+		return new HashMap<InvetoryObject, Integer>();
+	}
+	
+	protected Map<InvetoryObject, Integer> getMap(Map<InvetoryObject, Integer> initialInventory){
+		return new HashMap<InvetoryObject, Integer>(initialInventory);
+	}
 }
---
+++ branches/TextBasedRPG/src/org/baltoaca/objects/InvetoryObject.java
@@ -0,0 +1,5 @@
+package org.baltoaca.objects;
+
+public class InvetoryObject {
+
+}
