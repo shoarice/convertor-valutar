c9777c32fa30612e0b7f9ec79dcde7a68ec0e467
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-13 12:56:50 +0000

    Partial implementation of the expToLevel mapper

--- branches/TextBasedRPG/src/org/baltoaca/stats/ExperienceToLevelMapper.java
+++ branches/TextBasedRPG/src/org/baltoaca/stats/ExperienceToLevelMapper.java
@@ -1,9 +1,23 @@
 package org.baltoaca.stats;
 
+import java.util.Arrays;
+
 public class ExperienceToLevelMapper {
 
+	private int[] levelToExperience;
+	
+	public ExperienceToLevelMapper(int[] levelToExperience){
+		this.levelToExperience = Arrays.copyOf(levelToExperience, levelToExperience.length);
+	}
+	
 	public int getLevelFromExperience(int experience) {
-		// TODO Auto-generated method stub
+		int length = levelToExperience.length;
+		
+		for(int i=0;i < length;i++){
+			if(levelToExperience[i] > experience)
+				return i-1;
+		}
+		
 		return 0;
 	}
 
