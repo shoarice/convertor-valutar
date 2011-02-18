2e0ee320e1b48b500cb8b94ae8600b031fcde373
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-18 10:40:19 +0000

    Basic implementation of InventorySlot

--- src/org/baltoaca/characters/InventorySlot.java
+++ src/org/baltoaca/characters/InventorySlot.java
@@ -1,5 +1,40 @@
 package org.baltoaca.characters;
 
-public class InventorySlot {
+import org.baltoaca.objects.InventoryObject;
 
+public class InventorySlot {
+	private int amount;
+	private InventoryObject object;
+	
+	
+	public InventorySlot(InventoryObject object) {
+		amount = 1;
+		this.object = object;
+	}
+
+
+	public InventorySlot(int ammount, InventoryObject object) {
+		this.amount = ammount;
+		this.object = object;
+	}
+	
+	public boolean hasObject(InventoryObject object){
+		if(this.object.equals(object))
+			return true;
+		else
+			return false;
+	}
+	
+	public void incrementAmount(){
+		amount++;
+	}
+	
+	public void decrementAmount(){
+		amount--;
+	}
+	
+	public void setAmountToZero(){
+		amount = 0;
+	}
+	
 }
