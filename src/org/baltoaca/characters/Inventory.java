b24b0166e80e5cd726751643dd5318122a5a3aac
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-02-18 10:28:24 +0000

    Completely redesigned Inventory class

--- src/org/baltoaca/characters/Inventory.java
+++ src/org/baltoaca/characters/Inventory.java
@@ -1,31 +1,20 @@
 package org.baltoaca.characters;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.baltoaca.objects.InvetoryObject;
 
 public class Inventory {
-	Map<InvetoryObject, Integer> objects;
-	int maxSlots;
+	private int maxSlots;
+	private List<InventorySlot> inventory;
 	
-	public Inventory(){
+	
+	public Inventory() {
 		maxSlots = 10;
-		objects = getMap();
 	}
-	
+
+
 	public Inventory(int maxSlots){
-		this();
 		this.maxSlots = maxSlots;
 	}
 	
-	//TODO create the methods that add and remove objects
-	
-	protected Map<InvetoryObject, Integer> getMap(){
-		return new HashMap<InvetoryObject, Integer>();
-	}
-	
-	protected Map<InvetoryObject, Integer> getMap(Map<InvetoryObject, Integer> initialInventory){
-		return new HashMap<InvetoryObject, Integer>(initialInventory);
-	}
 }
