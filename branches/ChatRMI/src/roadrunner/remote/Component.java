e4fa67ec3d3ee567fd20cdc284ab421f06dbdbf3
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-04-02 17:32:42 +0000

    Adaugat comentarii

--- branches/ChatRMI/src/roadrunner/remote/Component.java
+++ branches/ChatRMI/src/roadrunner/remote/Component.java
@@ -1,5 +1,12 @@
 package roadrunner.remote;
 
-public interface Component {
+import java.rmi.Remote;
+
+/**
+ * The interface that will be available for remote method invocation on a component
+ * @author VlaD
+ *
+ */
+public interface Component extends Remote{
 
 }
