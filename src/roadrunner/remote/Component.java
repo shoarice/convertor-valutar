255cad2a649e6e34c8934016a55404961ac14bfd
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-04-04 17:07:11 +0000

    Adaugat noi metode pentru useri

--- src/roadrunner/remote/Component.java
+++ src/roadrunner/remote/Component.java
@@ -2,6 +2,9 @@ package roadrunner.remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Set;
+
+import roadrunner.model.User;
 
 /**
  * The interface that will be available for remote method invocation on a component
@@ -13,4 +16,18 @@ public interface Component extends Remote{
 	 * Method called by the central program when a new component connects to the swarm :P
 	 */
 	public void updateComponents() throws RemoteException;
+	
+	/**
+	 * Method called by the other components when the user list changes
+	 * @param component
+	 * @throws RemoteException
+	 */
+	public void updateUsers(Component component) throws RemoteException;
+	
+	/**
+	 * Method called by other components when this component calls updateUsers() on the other component
+	 * @return
+	 * @throws RemoteException
+	 */
+	public Set<User> getUsers() throws RemoteException;
 }
