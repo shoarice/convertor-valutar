d972a4cd8eaee8c8e8d0a3903e7b9ce813780736
ciprian.gurban <ciprian.gurban@gmail.com>
2011-04-05 16:38:48 +0000

    Ctrl Shift F

--- src/roadrunner/gui/ComponentFrame.java
+++ src/roadrunner/gui/ComponentFrame.java
@@ -16,7 +16,8 @@ import org.dyno.visual.swing.layouts.GroupLayout;
 import org.dyno.visual.swing.layouts.Leading;
 
 //VS4E -- DO NOT REMOVE THIS LINE!
-public class ComponentFrame extends JFrame {
+public class ComponentFrame extends JFrame
+{
 
 	private static final long serialVersionUID = 1L;
 	private JButton buttonApasa;
@@ -24,20 +25,28 @@ public class ComponentFrame extends JFrame {
 	private JScrollPane jScrollPane0;
 	private JPanel panelLogo;
 	private static final String PREFERRED_LOOK_AND_FEEL = "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel";
-	public ComponentFrame() {
+
+	public ComponentFrame()
+	{
 		initComponents();
 	}
 
-	private void initComponents() {
+	private void initComponents()
+	{
 		setLayout(new GroupLayout());
-		add(getJScrollPane0(), new Constraints(new Leading(14, 167, 10, 10), new Leading(14, 220, 10, 10)));
-		add(getPanelLogo(), new Constraints(new Leading(16, 76, 12, 12), new Leading(240, 60, 12, 12)));
-		add(getButtonApasa(), new Constraints(new Leading(102, 79, 12, 12), new Leading(240, 60, 12, 12)));
+		add(getJScrollPane0(), new Constraints(new Leading(14, 167, 10, 10),
+				new Leading(14, 220, 10, 10)));
+		add(getPanelLogo(), new Constraints(new Leading(16, 76, 12, 12),
+				new Leading(240, 60, 12, 12)));
+		add(getButtonApasa(), new Constraints(new Leading(102, 79, 12, 12),
+				new Leading(240, 60, 12, 12)));
 		setSize(193, 319);
 	}
 
-	private JPanel getPanelLogo() {
-		if (panelLogo == null) {
+	private JPanel getPanelLogo()
+	{
+		if (panelLogo == null)
+		{
 			panelLogo = new JPanel();
 			panelLogo.setLayout(new GroupLayout());
 			panelLogo.setBackground(Color.PINK);
@@ -45,55 +54,66 @@ public class ComponentFrame extends JFrame {
 		return panelLogo;
 	}
 
-	private JScrollPane getJScrollPane0() {
-		if (jScrollPane0 == null) {
+	private JScrollPane getJScrollPane0()
+	{
+		if (jScrollPane0 == null)
+		{
 			jScrollPane0 = new JScrollPane();
 			jScrollPane0.setViewportView(getListLocalUsers());
 		}
 		return jScrollPane0;
 	}
 
-	public JList getListLocalUsers() {
-		if (listLocalUsers == null) {
+	public JList getListLocalUsers()
+	{
+		if (listLocalUsers == null)
+		{
 			listLocalUsers = new JList();
 			DefaultListModel listModel = new DefaultListModel();
 			listLocalUsers.setModel(listModel);
-			
+
 		}
 		return listLocalUsers;
 	}
 
-	public JButton getButtonApasa() {
-		if (buttonApasa == null) {
+	public JButton getButtonApasa()
+	{
+		if (buttonApasa == null)
+		{
 			buttonApasa = new JButton();
 			buttonApasa.setText("Apasa");
 		}
 		return buttonApasa;
 	}
 
-	private static void installLnF() {
-		try {
+	private static void installLnF()
+	{
+		try
+		{
 			String lnfClassname = PREFERRED_LOOK_AND_FEEL;
 			if (lnfClassname == null)
 				lnfClassname = UIManager.getCrossPlatformLookAndFeelClassName();
 			UIManager.setLookAndFeel(lnfClassname);
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			System.err.println("Cannot install " + PREFERRED_LOOK_AND_FEEL
 					+ " on this platform:" + e.getMessage());
 		}
 	}
 
 	/**
-	 * Main entry of the class.
-	 * Note: This class is only created so that you can easily preview the result at runtime.
-	 * It is not expected to be managed by the designer.
-	 * You can modify it as you like.
+	 * Main entry of the class. Note: This class is only created so that you can
+	 * easily preview the result at runtime. It is not expected to be managed by
+	 * the designer. You can modify it as you like.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		installLnF();
-		SwingUtilities.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable()
+		{
 			@Override
-			public void run() {
+			public void run()
+			{
 				ComponentFrame frame = new ComponentFrame();
 				frame.setDefaultCloseOperation(ComponentFrame.EXIT_ON_CLOSE);
 				frame.setTitle("Component User List");
