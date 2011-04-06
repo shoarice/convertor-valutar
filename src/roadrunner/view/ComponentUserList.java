a69b9f2d18db0beb3fd2eb0997d6704a6e6bacfe
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-04-06 08:51:46 +0000

    Modificat clasele care se ocupa de update de GUI sa extinda View

---
+++ src/roadrunner/view/ComponentUserList.java
@@ -0,0 +1,31 @@
+package roadrunner.view;
+
+import java.util.Observable;
+
+import javax.swing.JList;
+import javax.swing.SwingUtilities;
+
+import roadrunner.model.Model;
+
+public class ComponentUserList extends View {
+
+	private JList list;
+	public ComponentUserList(Model model, JList list) {
+		super(model);
+		this.list=list;
+	}
+
+	@Override
+	public void update(Observable arg0, final Object arg1) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				list.setListData((Object[])arg1);
+			}
+		});
+	}
+	
+	
+
+}
