b10ed3955070a75b8fa98ad8c16c2f7327d27c6f
ammi.vlad <ammi.vlad@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-04-11 18:16:32 +0000

    Adugat actiune pentru butonul de logout. Logout-ul unui user inchide si toate ferestrele de chat ale acestuia

--- src/roadrunner/controller/client/ClientExitController.java
+++ src/roadrunner/controller/client/ClientExitController.java
@@ -1,5 +1,7 @@
 package roadrunner.controller.client;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -29,6 +31,15 @@ public class ClientExitController extends Controller {
 				component.removeLocalUser(username);
 			}
 			
+		});
+		
+		frame.getButtonLogout().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				component.removeLocalUser(username);
+				frame.dispose();
+			}
 		});
 	}
 	
