5016df7d495ca7c2aa9e083efcb8328ad9606fd0
ammi.vlad <ammi.vlad@gmail.com>
2011-09-16 14:26:49 +0000

    Initial cleanup

--- TextBasedRPG/src/org/baltoaca/CommandLineParser/Executor.java
+++
@@ -1,5 +0,0 @@
-package org.baltoaca.CommandLineParser;
-
-public interface Executor {
-	String executeAndGenerateResponse(String[] commandElements);
-}
--- TextBasedRPG/src/org/baltoaca/characters/EquippedSet.java
+++
@@ -1,5 +0,0 @@
-package org.baltoaca.characters;
-
-public class EquippedSet {
-
-}
