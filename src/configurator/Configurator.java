43b33bf09bdb35ee9fd6e17453f6e61a3325ee9f
ammi.vlad <ammi.vlad@gmail.com>
2011-09-19 17:06:29 +0000

    Implemented the configurator (workspace dependent - will work with ant build)

--- src/configurator/Configurator.java
+++ src/configurator/Configurator.java
@@ -3,6 +3,6 @@ package configurator;
 public class Configurator {
 	public static void main(String[] args) {
 		ConfigModel model = new ConfigModel();
-		Gui window = new Gui(model);
+		new Gui(model);
 	}
 }
