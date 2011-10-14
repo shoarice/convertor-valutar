e97e051ebe1bec41af32622acc0dd8d6a405edb6
ammi.vlad@gmail.com <ammi.vlad@gmail.com@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-10-14 21:46:12 +0000

    adaugat structura de date de baza

---
+++ branches/PCBE1/src/roadrunner/server/data/ServerData.java
@@ -0,0 +1,65 @@
+package roadrunner.server.data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ServerData {
+	private static ServerData instance = new ServerData();
+	public static ServerData instance(){
+		return instance;
+	}
+	
+	
+	private List<String> namesTaken;
+	private Map<Integer, String> clients; //asta va contine informatiile despre client (nume, queue etc). momentan doar numele
+	
+	private ServerData(){
+		namesTaken = Collections.synchronizedList(new ArrayList<String>());
+		clients = Collections.synchronizedMap(new HashMap<Integer, String>());
+	}
+	
+	public void addClient(int id, String name){
+		clients.put(id, name);
+		addNameTaken(name);
+	}
+	
+	public String getClient(int id){
+		return clients.get(id);
+	}
+	
+	public void removeClient(int id){
+		String name = clients.remove(id);
+		
+		if(name != null)
+			removeNameTaken(name);
+	}
+	
+	private void addNameTaken(String name){
+		namesTaken.add(name);
+		synchronized (namesTaken) {
+			Collections.sort(namesTaken);
+		}
+	}
+	
+	public boolean isNameTaken(String name){
+		synchronized (namesTaken) {
+			if(Collections.binarySearch(namesTaken, name) < 0)
+				return false;
+			
+			return true;			
+		}
+	}
+	
+	private void removeNameTaken(String name){
+		int i=0;
+		synchronized (namesTaken) {
+			if((i=Collections.binarySearch(namesTaken, name)) < 0)
+				return;
+			
+			namesTaken.remove(i);			
+		}
+	}
+}
