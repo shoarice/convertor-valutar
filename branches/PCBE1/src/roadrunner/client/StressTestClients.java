871c13acd6217a66d5c6ef6acbaf1ee4d768741c
ammi.vlad@gmail.com <ammi.vlad@gmail.com@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-10-16 08:52:32 +0000

    adaugat un stress test cu clienti automati

---
+++ branches/PCBE1/src/roadrunner/client/StressTestClients.java
@@ -0,0 +1,53 @@
+package roadrunner.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+public class StressTestClients {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=0;i < 100;i++){
+			new Thread(){
+
+				@Override
+				public void run() {
+					try {
+						Socket sock = new Socket("127.0.0.1", 49999);
+						BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+						PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+						Random rand = new Random();
+						
+						while(true){
+							if(rand.nextInt(3) == 0){
+								out.println("sendmsg topic "+rand.nextInt(5) +" "+ 1+rand.nextInt(5));
+								in.readLine();
+								out.println("sendmsg mesaj");
+								in.readLine();
+							}
+							else{
+								out.println("readmsg topic "+rand.nextInt(5));
+								in.readLine();
+							}
+						}
+					} catch (UnknownHostException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}.start();
+		}
+			
+	}
+
+}
