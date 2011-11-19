2fc284aa382de191b71fef50b08722d5c572733c
ciprian.gurban <ciprian.gurban@gmail.com>
2011-11-19 21:28:38 +0000

    GUI - update la Writer's Center

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -3,14 +3,8 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.Color;
@@ -51,51 +45,30 @@ public class Login {
 		frmLogin.setTitle("Welcome");
 		frmLogin.setBounds(100, 100, 274, 226);
 		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{49, 158, 0};
-		gridBagLayout.rowHeights = new int[]{45, 0, 0, 16, 0, 16, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frmLogin.getContentPane().setLayout(gridBagLayout);
+		frmLogin.getContentPane().setLayout(null);
 		
 		JLabel lblWelcome = new JLabel("Welcome to");
-		GridBagConstraints gbc_lblWelcome = new GridBagConstraints();
-		gbc_lblWelcome.anchor = GridBagConstraints.NORTH;
-		gbc_lblWelcome.insets = new Insets(0, 0, 5, 0);
-		gbc_lblWelcome.gridx = 1;
-		gbc_lblWelcome.gridy = 1;
-		frmLogin.getContentPane().add(lblWelcome, gbc_lblWelcome);
+		lblWelcome.setBounds(101, 14, 73, 16);
+		frmLogin.getContentPane().add(lblWelcome);
 		
 		JLabel lblRoadrunnerNewscenter = new JLabel("RoadRunner NewsCenter");
+		lblRoadrunnerNewscenter.setBounds(50, 47, 178, 17);
 		lblRoadrunnerNewscenter.setForeground(new Color(123, 104, 238));
 		lblRoadrunnerNewscenter.setFont(new Font("Lucida Grande", Font.BOLD, 14));
-		GridBagConstraints gbc_lblRoadrunnerNewscenter = new GridBagConstraints();
-		gbc_lblRoadrunnerNewscenter.insets = new Insets(0, 0, 5, 0);
-		gbc_lblRoadrunnerNewscenter.gridx = 1;
-		gbc_lblRoadrunnerNewscenter.gridy = 2;
-		frmLogin.getContentPane().add(lblRoadrunnerNewscenter, gbc_lblRoadrunnerNewscenter);
+		frmLogin.getContentPane().add(lblRoadrunnerNewscenter);
 		
 		JLabel lblWhatDoYou = new JLabel("What do you want to do ?");
+		lblWhatDoYou.setBounds(59, 87, 158, 16);
 		lblWhatDoYou.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblWhatDoYou = new GridBagConstraints();
-		gbc_lblWhatDoYou.insets = new Insets(0, 0, 5, 0);
-		gbc_lblWhatDoYou.gridx = 1;
-		gbc_lblWhatDoYou.gridy = 4;
-		frmLogin.getContentPane().add(lblWhatDoYou, gbc_lblWhatDoYou);
+		frmLogin.getContentPane().add(lblWhatDoYou);
 		
 		JButton btnReadButton = new JButton("Read");
-		GridBagConstraints gbc_btnReadButton = new GridBagConstraints();
-		gbc_btnReadButton.insets = new Insets(0, 0, 5, 0);
-		gbc_btnReadButton.gridx = 1;
-		gbc_btnReadButton.gridy = 6;
-		frmLogin.getContentPane().add(btnReadButton, gbc_btnReadButton);
+		btnReadButton.setBounds(59, 118, 158, 29);
+		frmLogin.getContentPane().add(btnReadButton);
 		
 		JButton btnWriteButton = new JButton("Write");
-		GridBagConstraints gbc_btnWriteButton = new GridBagConstraints();
-		gbc_btnWriteButton.insets = new Insets(0, 0, 5, 0);
-		gbc_btnWriteButton.gridx = 1;
-		gbc_btnWriteButton.gridy = 7;
-		frmLogin.getContentPane().add(btnWriteButton, gbc_btnWriteButton);
+		btnWriteButton.setBounds(59, 159, 158, 29);
+		frmLogin.getContentPane().add(btnWriteButton);
 	}
 
 }
