a8da0d6b91514038cd37cbd967aebafd21b43b11
ammi.vlad@gmail.com <ammi.vlad@gmail.com@9f13d99a-d518-3c76-0f97-388cd632b04a>
2011-11-20 20:02:16 +0000

    Un fel de api peste JMS

---
+++ branches/PCBE2/src/model/UnmarshallerStiri.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+public class UnmarshallerStiri {
+
+	public Stire unmarshall(TextMessage txtMsg) throws JMSException{
+		
+		Stire stire = new Stire();
+		stire.setStire(txtMsg.getText());
+		stire.setAutor(txtMsg.getStringProperty("autor"));
+		stire.setSursa(txtMsg.getStringProperty("sursa"));
+		stire.setDataCreat(txtMsg.getStringProperty("creat"));
+		stire.setDataModificat(txtMsg.getStringProperty("modificat"));
+		
+		return stire;
+	}
+}
