a0093276e2b3a49bc8d99247d4d2caeb5e853fa3
ciprian.gurban@gmail.com <ciprian.gurban@gmail.com@9f13d99a-d518-3c76-0f97-388cd632b04a>
2012-03-03 15:53:02 +0000

    legatura cu paginile

--- branches/SMA/src/org/sma/bigbrother/BigBrother.java
+++ branches/SMA/src/org/sma/bigbrother/BigBrother.java
@@ -17,6 +17,70 @@ public class BigBrother extends Activity {
 			startActivity(batteryIntent);
 		}
 	};
+	
+	private OnClickListener networkListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent networkIntent = new Intent(BigBrother.this, Network.class);
+			startActivity(networkIntent);
+		}
+	};
+	
+	private OnClickListener sensorsListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent sensorsIntent = new Intent(BigBrother.this, Sensors.class);
+			startActivity(sensorsIntent);
+		}
+	};
+	
+	private OnClickListener processesListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent processesIntent = new Intent(BigBrother.this, Processes.class);
+			startActivity(processesIntent);
+		}
+	};
+	
+	private OnClickListener telephonyListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent telephonyIntent = new Intent(BigBrother.this, Telephony.class);
+			startActivity(telephonyIntent);
+		}
+	};
+	
+	private OnClickListener locationListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent locationIntent = new Intent(BigBrother.this, Location.class);
+			startActivity(locationIntent);
+		}
+	};
+	
+	private OnClickListener logListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent logIntent = new Intent(BigBrother.this, Log.class);
+			startActivity(logIntent);
+		}
+	};
+	
+	private OnClickListener aboutListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent aboutIntent = new Intent(BigBrother.this, About.class);
+			startActivity(aboutIntent);
+		}
+	};
+	
     
 	/** Called when the activity is first created. */
     @Override
@@ -34,11 +98,13 @@ public class BigBrother extends Activity {
         Button About = (Button) findViewById(R.id.About); 
        
         Battery.setOnClickListener(batteryListener);
-        
-       
-	
-	
-      
-        
+        Network.setOnClickListener(networkListener);
+        Sensors.setOnClickListener(sensorsListener);
+        Processes.setOnClickListener(processesListener);
+        Telephony.setOnClickListener(telephonyListener);
+        Location.setOnClickListener(locationListener);
+        Log.setOnClickListener(logListener);
+        About.setOnClickListener(aboutListener);
+
     }
 }
