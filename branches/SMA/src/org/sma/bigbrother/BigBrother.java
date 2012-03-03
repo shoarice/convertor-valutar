ce9abb33bbf6941e3cd7ac90597a4155d7e8af80
ciprian.gurban@gmail.com <ciprian.gurban@gmail.com@9f13d99a-d518-3c76-0f97-388cd632b04a>
2012-03-03 14:46:13 +0000

    Main window - battery

--- branches/SMA/src/org/sma/bigbrother/BigBrother.java
+++ branches/SMA/src/org/sma/bigbrother/BigBrother.java
@@ -4,39 +4,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class BigBrother extends Activity {
     private static final String TAG = "BigBrother";
     private List<Sensor> sensors = new ArrayList<Sensor>();
+    	
+    private OnClickListener batteryListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Intent batteryIntent = new Intent(BigBrother.this, Battery.class);
+			startActivity(batteryIntent);
+		}
+	};
     
-	
     //private SensorManagerSimulator mSensorManager;
-    private SensorManager mSensorManager;
+    //private SensorManager mSensorManager;
     
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                      
+        Button Battery = (Button) findViewById(R.id.Battery);
+        Button Network = (Button) findViewById(R.id.Network);
+        Button Sensors = (Button) findViewById(R.id.Sensors);
+        Button Processes = (Button) findViewById(R.id.Processes);
+        Button Telephony = (Button) findViewById(R.id.Telephony);
+        Button Location = (Button) findViewById(R.id.Location);
+        Button Log = (Button) findViewById(R.id.Log);
+        Button About = (Button) findViewById(R.id.About); 
+       
+        Battery.setOnClickListener(batteryListener);
         
-        
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+       
+	
+	
+       /* mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         
         //CU SensorSimulator
-       /* mSensorManager = SensorManagerSimulator.getSystemService(this, SENSOR_SERVICE);
+        mSensorManager = SensorManagerSimulator.getSystemService(this, SENSOR_SERVICE);
         mSensorManager.connectSimulator();
-        List<Integer> deviceSensors = mSensorManager.getSensors();*/
+        List<Integer> deviceSensors = mSensorManager.getSensors();
         
         Log.d(TAG, deviceSensors.toString());
         for (Sensor s : deviceSensors) {
         	sensors.add(s);
-		}
+		}*/
         
         
     }
