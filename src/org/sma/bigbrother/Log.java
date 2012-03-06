92a31755d0a28e17c9c0444be61cd0f41d2ed6bc
ammi.vlad <ammi.vlad@gmail.com>
2012-03-06 23:31:28 +0000

    location activity

--- src/org/sma/bigbrother/Log.java
+++ src/org/sma/bigbrother/Log.java
@@ -1,5 +1,11 @@
 package org.sma.bigbrother;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,5 +16,21 @@ public class Log extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.log);
+		
+		File f = new File("/dev/log/main");
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(f));
+			String x = null;
+			do{
+				 x = in.readLine();
+				android.util.Log.d("BB", x);
+			}
+			while(x != null);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
